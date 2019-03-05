518e2256d750bf533bd6a2005bd326ba82ae71f1
Corentin Jabot <corentin.jabot@mobsya.org>
2019-03-05 14:27:36 +0100

    Add translations

--- browser.js
+++ browser.js
@@ -1,15 +1,42 @@
 var Blockly = require('./lib/blockly_compressed_browser');
 
 Blockly.setLocale = function(locale) {
-  Blockly.Msg = Object.assign(locale, Blockly.Msg);
-  Blockly.Msg = Blockly.Msg();
+    Blockly.Msg = Object.assign(locale, Blockly.Msg);
+    Blockly.Msg = Blockly.Msg();
 }
 
 Blockly.utils.getMessageArray_ = function () {
-  return Blockly.Msg
+    return Blockly.Msg
 }
+try {
+    var file = window.location.toString()
+    var re = new RegExp('.*\.([a-z]{2})\.html');
+    console.log(file)
+    var language = file.match(re)[1];
+    if (!language) {
 
-Blockly.setLocale(require('./lib/i18n/en'))
+        if (window.navigator.languages) {
+            language = window.navigator.languages[0];
+        } else {
+            language = window.navigator.userLanguage || window.navigator.language;
+        }
+        language = language.substring(0, 2);
+    }
+
+    if(language == 'de')
+        Blockly.setLocale(require('./lib/i18n/de'))
+    else if(language == 'fr')
+        Blockly.setLocale(require('./lib/i18n/fr'))
+    else if(language == 'es')
+        Blockly.setLocale(require('./lib/i18n/es'))
+    else if(language == 'it')
+        Blockly.setLocale(require('./lib/i18n/it'))
+    else
+        Blockly.setLocale(require('./lib/i18n/en'))
+}
+catch(e) {
+    Blockly.setLocale(require('./lib/i18n/en'))
+}
 
 Blockly.Blocks = Object.assign(Blockly.Blocks, require('./lib/blocks_compressed_browser')(Blockly));
 
